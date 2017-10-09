7ff33c73581f7d4629575c1cc0822ed90adee203
Will Soto <will.soto9@gmail.com>
2017-10-09 08:24:10 -0400

    feat(all): add health-check
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const version = require('../package.json').version;
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.use(morgan('dev'));
 
 app.use('/api/languages', require('./languages'));
 app.use('/api/syntax-themes', require('./syntax-themes'));
+
+app.use('/api/health-check', function(req, res) {
+  return res.json({
+    status: 'healthy',
+    version: version
+  });
+});
 
 app.listen(PORT, function() {
   // eslint-disable-next-line no-console
