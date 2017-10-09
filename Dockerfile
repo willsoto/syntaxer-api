570aa16ef8521d2f612702a834ddc9e950f8efbf
Will Soto <will.soto9@gmail.com>
2017-10-08 21:13:49 -0400

    feat(Dockerfile): make PORT configurable via environment
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,11 @@ FROM node:8
 ENV NPM_CONFIG_LOGLEVEL warn
 ENV APP_DIR /app
 ENV NODE_ENV production
+ENV PORT 8080
 
 ADD . $APP_DIR
 WORKDIR $APP_DIR
 
 RUN ["npm", "install"]
+
+EXPOSE $PORT
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const morgan = require('morgan');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(morgan('dev'));
 
 app.use('/api/languages', require('./languages'));
 app.use('/api/syntax-themes', require('./syntax-themes'));
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   // eslint-disable-next-line no-console
   console.log('Listening on :3000');
 });
